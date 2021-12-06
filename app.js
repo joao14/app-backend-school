bae993d4533d82d1b8ef7fbee7969b9aa44545c8
joao14 <barcealex1014@hotmail.com>
2021-12-05 22:43:03 -0500

    Adding new configuration pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World Alex Merino');
+    res.send('Hello World Alex Merino v1.1');
 })
 
 app.listen(3000, function() {
