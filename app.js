a50207b591baa79c28748a8712679b03148d354e
joao14 <barcealex1014@hotmail.com>
2021-12-11 04:12:36 -0500

    Added configuration

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var cors = require('cors');
 app.use(express.json());
 const PORT = process.env.PORT || 3000
+
+app.use(cors())
 
 //Import routes 
 var userRoutes = require('./src/routes/user.routes')
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const checkAuth = require('../middleware/checkAuth.middleware');
 const userControllers = require('../controllers/user.controllers');
-
+var cors = require('cors')
 const router = express.Router();
 
-router.post('/login', userControllers.userLogin);
-router.post('/add', checkAuth.validate, userControllers.add);
-router.get('/all', checkAuth.validate, userControllers.getUsers);
-router.put('/update', checkAuth.validate, userControllers.update);
-router.delete('/delete/:id', checkAuth.validate, userControllers.remove);
+var allowlist = ['http://localhost:3000/', 'http://example2.com']
+var corsOptionsDelegate = function(req, callback) {
+    var corsOptions;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+router.post('/login', cors(corsOptionsDelegate), userControllers.userLogin);
+router.post('/add', cors(corsOptionsDelegate), checkAuth.validate, userControllers.add);
+router.get('/all', cors(corsOptionsDelegate), checkAuth.validate, userControllers.getUsers);
+router.put('/update', cors(corsOptionsDelegate), checkAuth.validate, userControllers.update);
+router.delete('/delete/:id', cors(corsOptionsDelegate), checkAuth.validate, userControllers.remove);
 
 
 module.exports = router
